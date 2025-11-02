10ccb3cff3a387bc7390b8e1a066b0d601aed7ea
Julia <juliakrivonos22@gmail.com>
2025-11-03 02:50:57 +0300

    refactor: make the file executable directly

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import readline from 'node:readline';
 import { parseStartupArgs, printCwd } from "./src/utils/utils.js";
 import { dispatch } from "./src/commands/dispatcher.js";
